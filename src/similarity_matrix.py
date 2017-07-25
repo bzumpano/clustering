6a0876b6c112e25f4b9c36f2c4b9181a6f2ffa30
bzumpano <zumpano.lp@gmail.com>
2017-07-25 02:06:23 -0300

    Matriz de similaridade

---
+++ src/similarity_matrix.py
@@ -0,0 +1,44 @@
+# import unittest
+from math import sqrt
+
+# Mede a distancia de 2 pontos a(xa, ya) e b(xb, yb)
+def distance(a, b):
+  xa = a[0]
+  xb = b[0]
+
+  ya = a[1]
+  yb = b[1]
+
+  dist = sqrt((xa-xb)**2) + ((ya-yb)**2)
+  # print('d(%s, %s)=%f' % (str(a), str(b), dist))
+  return dist
+
+
+#
+# Regra:
+#
+# Returns:
+#   1, se distance(a, b) < epsilon
+#   0, c.c
+#
+def similarity_value(dist):
+  epsilon = 1.1
+
+  return 1 if dist < epsilon else 0
+
+
+
+def similarity_matrix(points):
+  matrix = []
+
+  for i in range(0, len(points)):
+    row = []
+
+    for j in range(0, len(points)):
+      dist = distance(points[i], points[j])
+      row.append(similarity_value(dist))
+
+    matrix.append(row)
+
+  return matrix
+
--- test/test_similarity_matrix.py
+++ test/test_similarity_matrix.py
@@ -1,1 +1,40 @@
 import unittest
+
+import sys
+sys.path.append('../src')
+
+from similarity_matrix import distance, similarity_value, similarity_matrix
+
+class TestSimilarityMatrix(unittest.TestCase):
+
+  def test_distance(self):
+    a = [0, 0]
+    b = [1, 0]
+
+    self.assertEquals(distance(a, a), 0)
+    self.assertEquals(distance(a, b), 1)
+
+
+  def test_similarity_value(self):
+    epsilon = 1.1
+
+    self.assertEquals(similarity_value(epsilon-1), 1)
+    self.assertEquals(similarity_value(epsilon), 0)
+    self.assertEquals(similarity_value(epsilon+1), 0)
+
+
+  def test_similarity_matrix(self):
+    points = [[0, 0], [1, 0], [1, 1], [0, 1]]
+    expected = [
+      [1, 1, 0, 1],
+      [1, 1, 1, 0],
+      [0, 1, 1, 1],
+      [1, 0, 1, 1]
+    ]
+
+    self.assertEquals(similarity_matrix(points), expected)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
